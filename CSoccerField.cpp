488a5090a5e98608a640ceec17ff4da9f2493646
Martin Bonilla <martinbonillasarmiento@gmail.com>
2026-03-02 16:13:31 -0500

    feat: initializarion of field

--- CSoccerField.cpp
+++ CSoccerField.cpp
@@ -1,2 +1,52 @@
 #include <CSoccerField.h>
+CSoccerField::CSoccerField()
+{
+    // GOAL SUPERIOR LINES
+    for (int j = 0; j < 5; j++)
+    {
+        matrix[19][j] = '#';
+        matrix[19][120-j] = '#';
+    }
 
+    // GOAL INFERIOR LINES
+    for (int j = 0; j < 5; j++)
+    {
+        matrix[31][j] = '#';
+        matrix[31][120-j] = '#';
+    }
+    
+    // GOAL VERTICAL LINES
+    for (int i = 0; i < 11; i++)
+    {
+        matrix[20+i][4] = '#';
+        matrix[20+i][116] = '#';
+    }
+    
+    // AREA SUPERIOR LINES
+    for (int j = 0; j < 10; j++)
+    {
+        matrix[12][j] = '#';
+        matrix[12][120-j] = '#';
+    }
+    
+    // AREA INFERIOR LINES
+    for (int j = 0; j < 10; j++)
+    {
+        matrix[38][j] = '#';
+        matrix[38][120-j] = '#';
+    }
+    
+    // AREA VERTICAL LINES
+    for (int i = 0; i < 25; i++)
+    {
+        matrix[13+i][9] = '#';
+        matrix[13+i][111] = '#';
+    }
+    
+    // TODO: AREA BOMB LINES
+
+}
+
+CSoccerField::~CSoccerField()
+{
+}
--- CSoccerField.h
+++ CSoccerField.h
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
+const int N_ROWS = 51;
+const int N_COLS = 121;
 class CSoccerField
 {
 private:
     string name;
-    int rows;
-    int cols;
+    int rows = N_ROWS;
+    int cols = N_COLS;
+    char matrix[N_ROWS][N_COLS];
 public:
-    CSoccerField(/* args */);
+    CSoccerField();
     ~CSoccerField();
-    void setRows(int _rows);
-    void setCols(int _cols);
 };
