c922565332e70c5821df8f10b8e8b312f454aaf2
Martin Bonilla <martinbonillasarmiento@gmail.com>
2026-03-02 17:43:42 -0500

    feat: setters and getters CPlayer

--- CPlayer.cpp
+++ CPlayer.cpp
@@ -1,11 +1,24 @@
 #include "CPlayer.h"
-CPlayer::CPlayer(/* args */)
+CPlayer::CPlayer()
 {
+    this->letter = ' ';
+    this->posX = 0;
+    this->posY = 0;
 }
 
-CPlayer::CPlayer(char l, int pX, int pY): letter(l), posX(pX), posY(pY)
+CPlayer::CPlayer(char l, int pX, int pY)
 {
+    this->letter = l;
+    this->posX = pX;
+    this->posY = pY;
 }
+
+char CPlayer::get_letter() const {return letter;}
+int CPlayer::get_posX() const {return posX;}
+int CPlayer::get_posY() const {return posY;}
+void CPlayer::set_letter(char _letter) {letter = _letter;}
+void CPlayer::set_posX(int _posX) {posX = _posX;}
+void CPlayer::set_posY(int _posY) {posY = _posY;}
 
 CPlayer::~CPlayer()
 {
--- CPlayer.h
+++ CPlayer.h
@@ -8,5 +8,11 @@ private:
 public:
     CPlayer();
     CPlayer(char l, int pX, int pY);
+    char get_letter() const;
+    int get_posX() const;
+    int get_posY() const;
+    void set_letter(char _letter);
+    void set_posX(int _posX);
+    void set_posY(int _posY);
     ~CPlayer();
 };
